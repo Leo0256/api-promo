e25cd046fe6b3c08f1047236750169859b8db5b3
Leonardo G. Ribeiro <leo.ribeiro0256@gmail.com>
2023-09-29 16:23:00 -0300

    login: login de administrador

--- src/models/usuario.js
+++ src/models/usuario.js
@@ -20,25 +20,15 @@ export default class Usuario {
      * @param {string} senha senha de acesso
      */
     static async login(login, senha) {
-        // Login do Admin (inativo)
-        /* const find_admin = await lltckt_user.findOne({
-            where: { username: login },
+        // Login do Admin
+        const find_admin = await lltckt_user.findOne({
+            where: { email: login, status: 1 },
             attributes: [
                 'user_id',
                 'password',
                 'salt'
             ]
         })
-
-        if(!!find_admin) {
-            let user = find_admin.get()
-
-            // Gera a criptografia da senha
-            let senha_compare = crypto.SHA1(senha).toString()
-            for (let i = 0; i < 2; i++) {
-                senha_compare = crypto.SHA1(user.salt + senha_compare).toString()
-            }
-        } */
 
         // Procura pelo Promotor
         const find_promo = await lltckt_manufacturer.findOne({
@@ -50,25 +40,51 @@ export default class Usuario {
         })
 
         // Usuário não encontrado
-        if(!find_promo) throw {
+        if(!find_promo && !find_admin) throw {
             status: 400,
             message: 'Usuário incorreto'
         }
 
-        // Gera a criptografia da senha
-        let senha_compare = crypto.MD5(senha).toString()
-        
-        // Senha incorreta?
-        if(find_promo.getDataValue('senha') !== senha_compare) throw {
-            status: 400,
-            message: 'Senha incorreta'
-        }
+        let senha_compare
+        let token
 
-        // Gera o token de acesso
-        let token = Auth.generateAuth(
-            false,
-            find_promo.getDataValue('manufacturer_id')
-        )
+        if(!!find_admin) {
+            let user = find_admin.get()
+
+            // Gera a criptografia da senha
+            let senha_compare = crypto.SHA1(senha).toString()
+            for (let i = 0; i < 2; i++) {
+                senha_compare = crypto.SHA1(user.salt + senha_compare).toString()
+            }
+
+            // Senha incorreta?
+            if(user.password !== senha_compare) throw {
+                status: 400,
+                message: 'Senha incorreta'
+            }
+
+            // Gera o token de acesso
+            token = Auth.generateAuth(
+                true,
+                '%'
+            )
+        }
+        else {
+            // Gera a criptografia da senha
+            senha_compare = crypto.MD5(senha).toString()
+
+            // Senha incorreta?
+            if(find_promo.getDataValue('senha') !== senha_compare) throw {
+                status: 400,
+                message: 'Senha incorreta'
+            }
+
+            // Gera o token de acesso
+            token = Auth.generateAuth(
+                false,
+                find_promo.getDataValue('manufacturer_id')
+            )
+        }
 
         return { token }
     }
